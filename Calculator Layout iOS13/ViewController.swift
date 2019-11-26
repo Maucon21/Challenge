9a61b8f95711f2316075b469f6aba3464e828e19
Mauricio Contreras <ingmaucon2102@gmail.com>
2019-11-26 16:22:25 -0600

    test

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -4,8 +4,7 @@
 //
 //  Created by Angela Yu on 01/07/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
+// Chaged Mauricio contreras 
 import UIKit
 
 class ViewController: UIViewController {
